d342ca363645ed54a78b289508997e4ae755612b
santhoshnarayanan09 <santhosh.aust@gmail.com>
2020-03-29 01:37:04 +0100

    basic codes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -67,5 +67,100 @@ public class Main {
         num = (byte) ((num1 > 10) ? 100 : 200);
         System.out.println("ternary num : " + num );
 
+      // Control Statements and relational Operators
+
+        // If Statement
+
+        if(num < num1) // Condition greater than
+        {
+            System.out.println("num is greater than num1"); // Statement
+        }
+
+        // Nested if
+
+        if (num < num1)
+        {
+            if (num1 > num) // Condition less than
+            {
+                System.out.println("num1 is greater");
+            }
+        }
+
+       // If else
+
+       if(num == num1) // Condition values are equal to
+       {
+           System.out.println("num and num 1 are equal");
+       }
+       else {
+           System.out.println("num and num1 are not equal");
+       }
+
+       //if-else-if
+
+        if(num1 >= num ) // Condition greater than or equal to
+        {
+            System.out.println("num1 is greater");
+        }
+        else if (num < 100 && num >= 10)
+        {
+            System.out.println("num is a two digit number");
+        }
+        else if (num != 0) // condition not equal to zero
+        {
+            System.out.println("num is a non-zero value");
+        }
+        else
+        {
+            System.out.println("value of num :" + num);
+        }
+
+        // Switch case
+
+        switch (a + 10)
+        {
+            case 1:
+                System.out.println("Value of a :" + a);
+            case 2:
+                System.out.println("value of a : " + a);
+            default:
+                System.out.println("Case");
+        }
+
+        // for loop
+
+        for (int j = 1; j < 10; j++){
+
+            System.out.println("value of i ;" + j);
+        }
+
+        //foreach to iterate in array
+
+        int arr[] = {1,2,3,4,5,6,7,8};
+        for (int i = 0; i < arr.length; i++)
+        {
+            System.out.println(arr[i]);
+        }
+
+        // Enhanced for loop
+
+        for (int i : arr){
+            System.out.println(i);
+        }
+
+        // while
+        int i = 1;
+        while (i < 10)
+        {
+            System.out.println("while");
+            i++;
+        }
+        // do while
+
+        do{
+            System.out.println("do..while");
+            i=100;
+        }while (i < 10);
+
     }
 }
